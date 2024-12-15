dd06baf82fedba355510ca3578660c7c9b91bb42
kristjan kangro <kristjan.kangro@visma.com>
2024-12-15 22:01:11 +0200

    reg

--- angular-app/src/app/home/home.component.ts
+++ angular-app/src/app/home/home.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Router} from '@angular/router';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    NgIf
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.less'
 })
 export class HomeComponent {
   title = 'Home';
+  showReg: boolean = false;
+
+  constructor(private router: Router) {
+    this.title = 'Home';
+    this.router = router;
+  }
+
+  btnClick() {
+    this.showReg = !this.showReg;
+  }
 }
