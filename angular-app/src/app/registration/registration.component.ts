2651a95e832d7496ef042e3258747747c07fdb1c
kristjan kangro <kristjan.kangro@visma.com>
2024-12-16 19:00:27 +0200

    just style

--- angular-app/src/app/registration/registration.component.ts
+++ angular-app/src/app/registration/registration.component.ts
@@ -2,18 +2,18 @@ import {Component, EventEmitter, Output} from '@angular/core';
 import {NgIf} from '@angular/common';
 
 @Component({
-  selector: 'app-registration',
-  imports: [
-    NgIf
-  ],
-  templateUrl: './registration.component.html',
-  styleUrl: './registration.component.less'
+    selector: 'app-registration',
+    imports: [
+        NgIf
+    ],
+    templateUrl: './registration.component.html',
+    styleUrl: './registration.component.less'
 })
 export class RegistrationComponent {
 
-  @Output('updateFormVisible)') updateFormVisible = new EventEmitter<boolean>();
+    @Output('updateFormVisible)') updateFormVisible = new EventEmitter<boolean>();
 
-  btnClick() {
-    this.updateFormVisible.emit(false);
-  }
+    btnClick() {
+        this.updateFormVisible.emit(false);
+    }
 }
