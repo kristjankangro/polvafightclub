33727f7320bfb902f2fbdccf701b952703a74f4d
kristjan kangro <kristjan.kangro@visma.com>
2024-12-16 22:55:32 +0200

    ...

--- vue-app-vite/src/components/MainNav/MainNav.vue
+++ vue-app-vite/src/components/MainNav/MainNav.vue
@@ -3,7 +3,7 @@ import "./MainNav.css";
 </script>
 
 <template>
-  <div class="main-nav flex-container column">
+  <div class="main-nav">
     <div><img class="logo-xl" src="/pvk-logo-bold.png" alt=""></div>
     <div class="nav-tab"><RouterLink to="/">Treeningplaan</RouterLink></div>
     <div class="nav-tab"><RouterLink to="/info">Kontakt</RouterLink></div>
